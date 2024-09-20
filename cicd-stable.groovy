db5e08b1b97e3c30a0ea6db5a632fd89bea9f02d
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:37:21 -0400

    Change organization to zopencommunity

--- cicd-stable.groovy
+++ cicd-stable.groovy
@@ -6,9 +6,9 @@ node('linux')
       branches: [[name: '*/main']],
       doGenerateSubmoduleConfigurations: false,
       extensions: [],
-      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/cunitport.git']]])
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/cunitport.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/cunitport.git'), string(name: 'PORT_DESCRIPTION', value: 'CUnit is a Unit testing framework for C.' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/cunitport.git'), string(name: 'PORT_DESCRIPTION', value: 'CUnit is a Unit testing framework for C.' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
   }
 }
